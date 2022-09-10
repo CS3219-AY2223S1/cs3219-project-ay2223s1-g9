74e4ea84e085058146be4e4fac8f2c29787231b8
LAPTOP-HR8PS1Q0\Admin <weishawtyhao9@gmail.com>
2022-09-10 14:48:03 +0800

    Implement backend matching service with difficulty levels

--- matching-service/index.js
+++ matching-service/index.js
@@ -1,17 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import { createServer } from 'http';
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { ormCreateMatch, ormDeleteMatchRoom } from "./model/matching-orm.js";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors()) // config cors so that front-end can use
-app.options('*', cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors()); // config cors so that front-end can use
+app.options("*", cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World from matching-service');
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.get("/", (req, res) => {
+  res.send("Hello World from matching-service");
 });
 
-const httpServer = createServer(app)
+io.on("connection", (socket) => {
+  console.log("New socket has been connected");
+  let roomId = "";
 
-httpServer.listen(8001);
+  socket.on("match", async ({ username, roomDifficulty }, callback) => {
+    const { matchRoom, err } = await ormCreateMatch({
+      username,
+      roomDifficulty,
+      roomId: socket.id,
+    });
+    if (err) {
+      return callback(err);
+    }
+    roomId = matchRoom.roomId;
+    socket.join(matchRoom.roomId);
+    // socket.emit("")
+  });
+
+  socket.on("disconnect", async () => {
+    if (roomId !== "") {
+      await ormDeleteMatchRoom(roomId);
+      // have to ask client side to disconnect.
+      // socket.broadcast.to(roomId).emit("client disconnect");
+    }
+  });
+});
+
+httpServer.listen(8001, () => {
+  console.log("App is listening on port 8001");
+});
