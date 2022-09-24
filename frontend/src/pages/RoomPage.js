5dedb7b719ae13d6debb3aa9cf9dfe03cc72cdf9
andrea-twl <tan.wanling.andrea@gmail.com>
2022-09-24 20:11:47 +0800

    add: username in room page

--- frontend/src/pages/RoomPage.js
+++ frontend/src/pages/RoomPage.js
@@ -1,5 +1,32 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext";
+
 const RoomPage = () => {
-  return <div>room?</div>;
+  const { user } = useContext(AuthContext); // contains user.username and user.token
+  console.log(user);
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      height={"100%"}
+      padding={"4rem"}
+      justifyContent={"center"}
+    >
+      <Typography>room?</Typography>
+      <Typography>user: {user.username ? user.username : ""}</Typography>
+    </Box>
+  );
 };
 
 export default RoomPage;
