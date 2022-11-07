dc236e67f9c7f79afc5874be73e40155d3195a87
andrea-twl <tan.wanling.andrea@gmail.com>
2022-11-08 02:24:27 +0800

    update: file for easier merging

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -13,23 +13,15 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useNavigate,
+  Navigate,
 } from "react-router-dom";
 import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import MatchingPage from "./MatchingPage";
-import StandardPage from "../components/templates/StandardPage";
-import BodyCopyLarge from "../components/atoms/BodyCopyLarge";
-import SecondaryNavBar from "../components/molecules/SecondaryNavBar";
-import styles from "./HomePage.module.scss";
-import Heading2 from "../components/atoms/Heading2";
-import PrimaryButton from "../components/atoms/PrimaryButton";
 
-const HomePage = () => {
+const ProblemsPage = () => {
   const [isMatching, setIsMatching] = useState(false);
   const [difficulty, setDifficulty] = useState("");
-  const navigate = useNavigate();
-
   const handleSelectDifficulty = (event) => {
     setIsMatching(true);
     setDifficulty(event.currentTarget.id);
@@ -39,34 +31,61 @@ const HomePage = () => {
   }, [isMatching]);
 
   return (
-    <StandardPage header={<SecondaryNavBar />}>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <Heading2>Collaborative problem solving </Heading2>
-          <BodyCopyLarge>
-            PEERPREP lets you practice solving popular technical interview
-            questions with your peers in real-time using a collaborative code
-            editor.
-          </BodyCopyLarge>
-          <div className={styles.content__button}>
-            <PrimaryButton
-              text={"start now"}
-              onClick={() => {
-                navigate("/login");
-              }}
-            />
-          </div>
+    <div>
+      {!isMatching && (
+        <div>
+          <NavBar />
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            height={"100%"}
+            padding={"4rem"}
+            justifyContent={"center"}
+          >
+            <Typography variant={"h6"} marginBottom={"10px"}>
+              Start coding!
+            </Typography>
+            <Typography>Select a difficulty level</Typography>
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              gap={"10px"}
+              justifyContent={"flex-end"}
+            >
+              <Button
+                id={"Easy"}
+                variant={"outlined"}
+                onClick={handleSelectDifficulty}
+              >
+                Easy
+              </Button>
+              <Button
+                id={"Medium"}
+                variant={"outlined"}
+                onClick={handleSelectDifficulty}
+              >
+                Medium
+              </Button>
+              <Button
+                id={"Hard"}
+                variant={"outlined"}
+                onClick={handleSelectDifficulty}
+              >
+                Hard
+              </Button>
+            </Box>
+          </Box>
         </div>
-        <div className={styles.image}>
-          <img
-            src={require("../assets/images/collab.png")}
-            width="493"
-            height="323"
-          />
-        </div>
-      </div>
-    </StandardPage>
+      )}
+      {isMatching && (
+        <MatchingPage
+          setIsMatching={setIsMatching}
+          difficulty={difficulty}
+          setDifficulty={setDifficulty}
+        />
+      )}
+    </div>
   );
 };
 
-export default HomePage;
+export default ProblemsPage;
---
+++ frontend/src/pages/HomePage2.js
@@ -0,0 +1,72 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MatchingPage from "./MatchingPage";
+import StandardPage from "../components/templates/StandardPage";
+import BodyCopyLarge from "../components/atoms/BodyCopyLarge";
+import SecondaryNavBar from "../components/molecules/SecondaryNavBar";
+import styles from "./HomePage.module.scss";
+import Heading2 from "../components/atoms/Heading2";
+import PrimaryButton from "../components/atoms/PrimaryButton";
+
+const HomePage = () => {
+  const [isMatching, setIsMatching] = useState(false);
+  const [difficulty, setDifficulty] = useState("");
+  const navigate = useNavigate();
+
+  const handleSelectDifficulty = (event) => {
+    setIsMatching(true);
+    setDifficulty(event.currentTarget.id);
+  };
+  useEffect(() => {
+    console.log(isMatching);
+  }, [isMatching]);
+
+  return (
+    <StandardPage header={<SecondaryNavBar />}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <Heading2>Collaborative problem solving </Heading2>
+          <BodyCopyLarge>
+            PEERPREP lets you practice solving popular technical interview
+            questions with your peers in real-time using a collaborative code
+            editor.
+          </BodyCopyLarge>
+          <div className={styles.content__button}>
+            <PrimaryButton
+              text={"start now"}
+              onClick={() => {
+                navigate("/login");
+              }}
+            />
+          </div>
+        </div>
+        <div className={styles.image}>
+          <img
+            src={require("../assets/images/collab.png")}
+            width="493"
+            height="323"
+          />
+        </div>
+      </div>
+    </StandardPage>
+  );
+};
+
+export default HomePage;
